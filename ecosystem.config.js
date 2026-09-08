811cdb72e65d7ab0e611f4ce30f08ea2b18df0e8
ladyman1 <ladyman1@users.noreply.github.com>
2026-09-08 13:08:09 +0100

    Update ecosystem.config.js to fork mode with 1 instance for SQLite

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       name: 'roofing-dashboard',
       script: 'src/server.js',
-      instances: 'max',
-      exec_mode: 'cluster',
+      instances: 1,
+      exec_mode: 'fork',
       autorestart: true,
       watch: false,
       max_memory_restart: '500M',
